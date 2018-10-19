a54a9b5e5ada86b9812e4a290c12a32a1ea08b2a
Alex Beaver <adbeaver@iastate.edu>
2018-10-18 20:37:29 -0500

    Added initial Race_Drael

--- Source/The_Yanorian_Rim.cs
+++ Source/The_Yanorian_Rim.cs
@@ -1,6 +1,17 @@
-﻿namespace The_Yanorian_Rim
+﻿using System;
+using RimWorld;
+using Verse;
+
+
+
+namespace The_Yanorian_Rim
 {
     public class The_Yanorian_Rim
     {
+        The_Yanorian_Rim()
+        {
+            
+        }
+
     }
 }
