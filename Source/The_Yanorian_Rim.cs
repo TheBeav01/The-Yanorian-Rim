51847230ae2cc2decfcb79bb107d0c7b210645fb
Alex Beaver <adbeaver@iastate.edu>
2018-10-24 22:22:59 -0500

    Began patching the pawn generation

--- Source/The_Yanorian_Rim.cs
+++ Source/The_Yanorian_Rim.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using System.Reflection;
 using RimWorld;
 using Verse;
-
+using Harmony;
+using UnityEngine;
+using System;
 
 
 namespace The_Yanorian_Rim
 {
-    public class The_Yanorian_Rim
-    {
-        The_Yanorian_Rim()
-        {
-            
-        }
 
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.HeartTheWolf.TheYanorianRim");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+		[HarmonyPatch(typeof(PawnGenerator))]
+		[HarmonyPatch("GeneratePawn")]
+		[HarmonyPatch(new Type[] { typeof(PawnGenerationRequest) })]
+        class TestPatch
+		{
+			static void Postfix(PawnGenerationRequest request)
+			{
+                Log.Message("Pawn Generated!");
+			}
+		}
     }
 }
