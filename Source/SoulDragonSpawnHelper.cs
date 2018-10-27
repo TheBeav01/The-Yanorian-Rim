85255025304a3e5fae544b43ef9da6a574e00826
Alex Beaver <adbeaver@iastate.edu>
2018-10-26 21:28:54 -0500

    Added spawn helper and mostly finished caravan

---
+++ Source/SoulDragonSpawnHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Verse;
+using RimWorld;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace The_Yanorian_Rim
+{
+    public class SoulDragonSpawnHelper
+    {
+        private PawnGenerationRequest req;
+        private PawnKindDef kindDef;
+        private string pawnRace;
+        private IList<string> races;
+        public SoulDragonSpawnHelper(PawnGenerationRequest request)
+        {
+            req = request;
+            kindDef = request.KindDef;
+            
+        }
+        public Boolean exec()
+        {
+            //TODO: Check if Drael race and spawn bonded dragon w/ hediff
+            if(kindDef.race.ToString() == "")
+            {
+
+            }
+            Log.Message("Race: " + kindDef.race.ToString());
+            return false;
+        }
+    }
+}
+
